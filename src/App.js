b4ed90045f1f1217f481c4f598390c8a200963d8
Satish Yadav <satish.yadav@nagarro.com>
2021-05-31 07:33:50 +0530

    Initial Counter App ready with styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [count, setCount] = useState(0);
+	const increment = () => setCount(count + 1);
+	const decrement = () => setCount(count - 1);
+	return (
+		<section className='app-container'>
+			<header className='app-header'>Counter App</header>
+			<dir className='counter-container'>
+				count<span className='count'>{count}</span>
+			</dir>
+			<dir className='btn-container'>
+				<button className='btn btn-inc' onClick={increment}>
+					Increment
+				</button>
+				<button className='btn btn-dec' onClick={decrement}>
+					Decrement
+				</button>
+			</dir>
+		</section>
+	);
 }
 
 export default App;
