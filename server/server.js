e206fb78d0e0698f3a65e32c85da8aaf9f5fb89c
Satish Yadav <satish.yadav@nagarro.com>
2021-05-31 08:28:35 +0530

    add static serve in server file import react in app file

--- server/server.js
+++ server/server.js
@@ -13,31 +13,29 @@ import ReactDOMServer from 'react-dom/server';
 import App from '../src/App';
 
 //Port where app will serve while running
-const PORT = 3000;
+const PORT = 8000;
 
 const app = express();
 
 //Serve app from root URI
-app.use('^/$', (req, res, next) => {
-	fs.readFile(
-		path.resolve('./build/index.html', 'utf-8', (err, data) => {
-			if (err) {
-				console.log(
-					'Error occure while reading file from build folder :::',
-					err
-				);
-				return res.status(500).send('Something went wrong on server!!!');
-			} else {
-				return res.send(
-					data.replace(
-						"<div id='root'></div>",
-						`<div id='root'>${ReactDOMServer.renderToString(<App />)}</div>`
-					)
-				);
-			}
-		})
-	);
+app.use('/', (req, res) => {
+	fs.readFile(path.resolve('./build/index.html'), 'utf-8', (err, data) => {
+		if (err) {
+			console.log('Error occure while reading file from build folder :::', err);
+			return res.status(500).send('Something went wrong on server!!!');
+		} else {
+			return res.send(
+				data.replace(
+					`<div id="root"></div>`,
+					`<div id="root">${ReactDOMServer.renderToString(<App />)}</div>`
+				)
+			);
+		}
+	});
 });
+
+//provide static path
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
 //Starting the server on CUSTOM PORT
 app.listen(PORT, () =>
